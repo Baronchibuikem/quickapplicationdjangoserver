0cac70257a0bd9eb2e033cc63113731368d4ceb6
baron <baronchibuike@gmail.com>
2022-01-10 11:13:17 +0100

    fixed bugs

--- quizbackend/settings.py
+++ quizbackend/settings.py
@@ -43,8 +43,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware', 
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
